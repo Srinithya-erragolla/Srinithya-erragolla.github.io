4c3b1630c383148b9dcd2e018559b0637d7eb3d8
Srinithya-erragolla <srinithyaerragolla441@gmail.com>
2026-01-10 15:42:14 +0530

    updated

--- html-css-jc/web-app-1/script.js
+++ html-css-jc/web-app-1/script.js
@@ -18,7 +18,7 @@ function showLoginForm(){
     let str = `
     <h3>Login Form</h3>
 
-      <p><label id="lblMessage"></label></p>
+     <p><label id="lblMessage"></label></p>
       <p><input type="text" id="txtEmail" /></p>
       <p><input type="password" id="txtPassword" /></p>
       <p><button class="login-btn" onclick="dispMessage()">Log In</button></p>
---
+++ html-css-jc/web-app-2/script.js
@@ -0,0 +1,20 @@
+const products=[
+    {
+        name:"Product 1",
+        desc:"This is a sample description",
+        price:45,
+        imgUrl: ""
+    },
+    {
+        name:"Product 2",
+        desc:"This is a sample description",
+        price:70,
+        imgUrl: ""
+    },
+    {
+        name:"Product 3",
+        desc:"This is a sample description",
+        price:95,
+        imgUrl: ""
+    }
+]
